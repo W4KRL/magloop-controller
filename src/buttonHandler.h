4d272d0afe5b145d02253c52ac27204b25540bcc
Karl Berger <W4KRL@arrl.net>
2025-03-22 18:59:56 -0400

    Added isJogActionAllowed

--- src/buttonHandler.h
+++ src/buttonHandler.h
@@ -1,6 +1,8 @@
 //! buttonHandler.h
 // 2025-03-16 improved interlocking of btn 3 and 4
 // 2025-03-18 comments expanded, actions renamed from action1, etc.
+// 2025-03-19 added inactiveButtonColor
+// 2025-03-20 added isJogActionAllowed()
 
 #ifndef BUTTONHANDLER_H
 #define BUTTONHANDLER_H
@@ -11,9 +13,9 @@
 #include "ledControl.h"
 #include "h_bridge.h"
 
-void actionScanUp();               // latching button, pressed toggles .depressed
-void actionScanDown();               // latching button
-void actionJogUp(String &action); // momentary button, action is "pressed" or "released"
+void actionScanUp();                // latching button, pressed toggles .depressed
+void actionScanDown();              // latching button
+void actionJogUp(String &action);   // momentary button, action is "pressed" or "released"
 void actionJogDown(String &action); // momentary button
 
 //! Structure to hold button state information
@@ -28,13 +30,13 @@ struct ButtonState
 //! Each entry includes: buttonId, initial state (depressed or not), and depressedColor (ON)
 //! button 0 is not used - it's just there as an index marker
 // button 1 and 2 are latching buttons
-// button 3 and 4 are momentary manual buttons
+// button 3 and 4 are momentary buttons
 ButtonState buttonStates[] = {
-    {"btn0", false, "dimGray"},
-    {"btn1", false, "skyblue"},
-    {"btn2", false, "lime"},
-    {"btn3", false, LED_DOWN_RED},
-    {"btn4", false, "yellow"}};
+    {"btn0", false, "dimGray"}, // not used
+    {"btn1", false, "skyblue"}, // scan up
+    {"btn2", false, "lime"},    // scan down
+    {"btn3", false, "red"},     // jog up
+    {"btn4", false, "yellow"}}; // jog down
 
 //! Color of all buttons when they are not pressed
 String inactiveButtonColor = "dimGray";
@@ -56,6 +58,12 @@ void updateButtonState(const String &buttonId)
   }
 } // updateButtonState()
 
+//! Helper function to check if jog actions are allowed
+bool isJogActionAllowed()
+{
+  return !buttonStates[1].depressed && !buttonStates[2].depressed;
+}
+
 //! Call the action responses for all buttons
 void buttonHandler(String &buttonId, String &action)
 {
@@ -68,11 +76,11 @@ void buttonHandler(String &buttonId, String &action)
   {
     actionScanDown();
   }
-  else if (buttonId == "btn3" && !buttonStates[1].depressed && !buttonStates[2].depressed)
-    {
-      actionJogUp(action);
-    }
-  else if (buttonId == "btn4" && !buttonStates[1].depressed && !buttonStates[2].depressed)
+  else if (buttonId == "btn3" && isJogActionAllowed())
+  {
+    actionJogUp(action);
+  }
+  else if (buttonId == "btn4" && isJogActionAllowed())
   {
     actionJogDown(action);
   }
