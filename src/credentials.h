db47b58bbea29b5aa667649826550b778e8e0e78
Karl Berger <W4KRL@arrl.net>
2025-04-04 16:14:40 -0400

    Cleanup comments

--- src/credentials.h
+++ src/credentials.h
@@ -8,6 +8,7 @@
 #define WIFI_SSID "DCMNET"
 #define WIFI_PASSWORD "0F1A2D3E4D5G6L7O8R9Y"
 // Set your Static IP address & hostname
+//! See instructions in Notes.md for setting up static IP address
 IPAddress localIP(192, 168, 0, 234);
 // Set your Gateway IP address
 IPAddress gateway(192, 168, 0, 1);
@@ -27,19 +28,20 @@ IPAddress subnet(255, 255, 255, 0);
 #define STYLES_DATE "2025-03-25"
 
 //! GPIO Pin definitions
-#define HB_IN1 23	  // pin IN1 logic
-#define HB_IN2 22	  // pin IN2 logic
-#define LIMIT_UP 19	  // pin upper limit switch
-#define LIMIT_DOWN 18 // pin lower limit switch
+#define HB_IN1 23	  // PWM IN1 logic
+#define HB_IN2 22	  // PWM IN2 logic
+#define LIMIT_UP 19	  // upper limit switch interrupt
+#define LIMIT_DOWN 18 // lower limit switch interrupt
 #define SWR_RFL 35	  // SWR reflected ADC input
 #define SWR_FWD 34	  // SWR forward ADC input
+#define VM 32		  // VM voltage monitor ADC input
 
 //! Web Button and LED colors
 // used in ledControl.h & actions.h
 #define LED_GREEN "LimeGreen"
 #define LED_RED "Red"
 // used in buttonControl.h
-#define SCAN_UP "RoyalBlue"	
+#define SCAN_UP "RoyalBlue"
 #define SCAN_DOWN "LimeGreen"
 #define JOG_UP "DodgerBlue"
 #define JOG_DOWN "Lime"
