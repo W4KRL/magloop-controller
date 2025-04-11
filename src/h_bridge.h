d0015cc42004f70be7216845988ed5b5a816ce4b
Karl Berger <W4KRL@arrl.net>
2025-04-11 11:46:09 -0400

    remove limit switch tests

--- src/h_bridge.h
+++ src/h_bridge.h
@@ -1,5 +1,5 @@
 //! h_bridge.h
-//! 2025-03-10
+//! 2025-04-11
 //! using fast stop inverted logic
 // see TI datasheet 7.3.1
 // https://www.ti.com/lit/ds/symlink/l298.pdf
@@ -7,10 +7,12 @@
 // ESP32 GPIOs DOIT ESP32 DEVKIT V1
 // tested with DRV8871 module
 
+//! test limit switches before call to setMotorSpeed()
+
 #ifndef H_BRIDGE_H
 #define H_BRIDGE_H
 
-#include <Arduino.h>
+#include <Arduino.h>     // for Arduino functions
 #include "credentials.h" // GPIO definitions
 
 #define pwmFrequency 1000 // frequency in Hz
@@ -18,11 +20,6 @@
 #define IDLE 0            // IDLE state
 #define MOVE_UP 1         // forward motion
 #define MOVE_DOWN 2       // reverse motion
-
-String motorDirString[] = {"Idle", "Move Up", "Move Down"};
-
-int motorDir = IDLE; // motor direction
-int motorSpeed = 0; // motor velocity
 
 void h_bridgeBegin()
 {
@@ -39,38 +36,34 @@ void h_bridgeBegin()
     ledcWrite(HB_IN2, 255);
 } // h-bridgeBegin()
 
-void setMotorSpeedDirect(int speed, int direction)
+void setMotorSpeed(int speed, int mode)
 {
-    motorDir = direction; // bag to let limit switches know motor direction
     //! H-Bridge outputs (fast stop inverted logic):
     //    as recommended by TI datasheet
     //    Forward mode IN1 = HIGH, IN1 =  PWM
     //    Reverse mode IN1 =  PWM, IN2 = HIGH
-    //    IDLEe    mode IN1 = HIGH, IN2 = HIGH
+    //    Idle    mode IN1 = HIGH, IN2 = HIGH
 
     //! NOTE: duty is inverted for inverted logic
     //!   pwm duty = 0 for full output!
     int duty = map(speed, 0, 100, 255, 0);
 
-    //! enable output to motor if it is not at a limit
-    switch (direction)
+    switch (mode)
     {
     case MOVE_UP:
-        if (!digitalRead(LIMIT_UP)) // not at upper limit
-        {
-            ledcWrite(HB_IN1, 255); // logic HIGH
-            ledcWrite(HB_IN2, duty);
-        }
+        ledcWrite(HB_IN1, 255);  // logic HIGH
+        ledcWrite(HB_IN2, duty); // inverted PWM
         break;
     case MOVE_DOWN:
-        if (!digitalRead(LIMIT_DOWN)) // not at lower limit
-        {
-            ledcWrite(HB_IN1, duty);
-            ledcWrite(HB_IN2, 255); // logic HIGH
-        }
+        ledcWrite(HB_IN1, duty); // inverted PWM
+        ledcWrite(HB_IN2, 255);  // logic HIGH
+        break;
+    case IDLE:
+        ledcWrite(HB_IN1, 255); // logic HIGH
+        ledcWrite(HB_IN2, 255); // logic HIGH
         break;
     default:
-        // direction is IDLE or undefined
+        // direction is undefined - set to idle
         ledcWrite(HB_IN1, 255); // logic HIGH
         ledcWrite(HB_IN2, 255); // logic HIGH
     }
