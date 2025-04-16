5e38a8986eafbc7d38801d62504f34c26a1ce604
Karl Berger <W4KRL@arrl.net>
2025-04-16 11:11:29 -0400

    Convert to debuging over WebSocket

--- src/debug.h
+++ src/debug.h
@@ -1,21 +1,16 @@
 //! debug.h
-// 2024-01-29
+// 2024-04-16
 
 //! !!!!! DO NOT AUTOFORMAT !!!!!
 /* 
-   Line 11 must look like this unbroken on one line:
-   #define DEBUG_PRINTF(...) Serial.printf("-> " __VA_ARGS__); Serial.print("\r\n")
-   Usage: DEBUG_PRINT("%s %s", "string", StringVariable.c_str());
+  In C++, a backslash (\) at the end of a line is used as a 
+  line continuation character in preprocessor directives (such as macros). 
+  It tells the preprocessor that the current line continues onto the next 
+  line, effectively joining them into a single logical line.
 */
 
 #ifndef DEBUG_MAGLOOP_H
 #define DEBUG_MAGLOOP_H
-
-// #ifdef DEBUG_MAGLOOP
-//   #define DEBUG_PRINTF(...) Serial.printf("-> " __VA_ARGS__); Serial.print("\r\n")
-// #else
-//   #define DEBUG_PRINTF(...) /* No-op: Debugging disabled */
-// #endif // DEBUG_MAGLOOP_H
 
 #ifdef DEBUG_MAGLOOP
   #define DEBUG_PRINTF(fmt, ...) do { \
@@ -28,5 +23,12 @@
 #else
   #define DEBUG_PRINTF(...) /* No-op: Debugging disabled */
 #endif // DEBUG_MAGLOOP
+
+//! Legacy Serial Monitor debug macro
+// #ifdef DEBUG_MAGLOOP
+//   #define DEBUG_PRINTF(...) Serial.printf("-> " __VA_ARGS__); Serial.print("\r\n")
+// #else
+//   #define DEBUG_PRINTF(...) /* No-op: Debugging disabled */
+// #endif // DEBUG_MAGLOOP_H
 
 #endif
