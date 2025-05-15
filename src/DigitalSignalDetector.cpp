a7529f38882f4bcd0bee7f550355bddab7283cda
Karl Berger <W4KRL@arrl.net>
2025-05-15 18:01:30 -0400

    added .cpp

---
+++ src/DigitalSignalDetector.cpp
@@ -0,0 +1,35 @@
+//! @file DigitalSignalDetector.cpp
+//! 2025-05-15
+// by Perplexity AI & Karl W. Berger, W4KRL
+
+#include "DigitalSignalDetector.h"
+
+DigitalSignalDetector::DigitalSignalDetector(int inputPin)
+    : pin(inputPin)
+{
+    pinMode(pin, INPUT);
+    currentState = static_cast<bool>(digitalRead(pin));
+    previousState = currentState;
+}
+
+void DigitalSignalDetector::update()
+{
+    previousState = currentState;
+    currentState = static_cast<bool>(digitalRead(pin));
+}
+
+bool DigitalSignalDetector::rose() const
+{
+    return (previousState == LOW && currentState == HIGH);
+}
+
+bool DigitalSignalDetector::fell() const
+{
+    return (previousState == HIGH && currentState == LOW);
+}
+
+bool DigitalSignalDetector::read() const
+{
+    return currentState;
+}
+// end of file
--- src/DigitalSignalDetector.h
+++ src/DigitalSignalDetector.h
@@ -1,7 +1,11 @@
 //! @file DigitalSignalDetector.h
 //! 2025-05-09
-
 // by Perplexity AI & Karl W. Berger, W4KRL
+
+#ifndef DIGITAL_SIGNAL_DETECTOR_H
+#define DIGITAL_SIGNAL_DETECTOR_H
+
+#include <Arduino.h>
 
 /**
  * @class DigitalSignalDetector
@@ -13,48 +17,22 @@
  * @note Call the `update()` method in your main loop to refresh the state of the pin.
  *       Call the constructor with the pin number to initialize the pin mode in setup().
  */
-
-#ifndef DIGITAL_SIGNAL_DETECTOR_H
-#define DIGITAL_SIGNAL_DETECTOR_H
-
-#include <Arduino.h>
-
 class DigitalSignalDetector
 {
 private:
-  int pin;
-  bool currentState;
-  bool previousState;
+    int pin;
+    bool currentState;
+    bool previousState;
 
 public:
-  DigitalSignalDetector(int inputPin)
-  {
-    pin = inputPin;
-    pinMode(pin, INPUT);
-    currentState = static_cast<bool>(digitalRead(pin));
-    previousState = currentState;
-  }
+    explicit DigitalSignalDetector(int inputPin);
 
-  void update()
-  {
-    previousState = currentState;
-    currentState = static_cast<bool>(digitalRead(pin));
-  }
+    void update();
 
-  bool rose() const
-  {
-    return (previousState == LOW && currentState == HIGH);
-  }
-
-  bool fell() const
-  {
-    return (previousState == HIGH && currentState == LOW);
-  }
-
-  bool read() const
-  {
-    return currentState;
-  }
+    bool rose() const;
+    bool fell() const;
+    bool read() const;
 };
 
 #endif // DIGITAL_SIGNAL_DETECTOR_H
+// end of file  
