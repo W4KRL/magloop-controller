538315f2dbd1f1c9aa8ed6266c1d3c7ec93c5b2c
Karl Berger <W4KRL@arrl.net>
2025-05-19 17:26:48 -0400

    Split into header/source

---
+++ src/swrCalc.cpp
@@ -0,0 +1,32 @@
+#include "swrCalc.h"
+
+#include <Arduino.h>
+#include "configuration.h" // for SWR pin definitions
+
+//! ADC pins are defined in configuration.h
+
+void swrCalcBegin()
+{
+  // setup the ADC pins
+  // call in setup()
+  pinMode(SWR_FWD_PIN, INPUT);
+  pinMode(SWR_RFL_PIN, INPUT);
+} // swrCalcBegin()
+
+float swrValue()
+{
+  // read the forward and reverse power
+  int fwdPower = analogRead(SWR_FWD_PIN);
+  int revPower = analogRead(SWR_RFL_PIN);
+
+  // calculate the SWR
+  float swr = (fwdPower + revPower) / (fwdPower - revPower);
+
+  return swr;
+} // swrValue()
+
+/*
+update the SWR continuously at 100 ms intervals
+send to the WebSocket only when the value has changed by more than 0.1
+
+*/
